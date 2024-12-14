6d497ecbd335c46fd921a802ca688773a559e68a
MoDiouf <mohameddiouf096@gmail.com>
2024-12-14 16:26:13 +0000

    ok

--- src/statistique/statistique.controller.ts
+++ src/statistique/statistique.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { StatistiqueService } from './statistique.service';
 
-@Controller('statistics')
+@Controller('app/statistics')
 export class StatistiqueController {
+  constructor(private readonly statistiqueService: StatistiqueService) {}
     @Get()
     @Render('user/dashboard')
-    getAddNetwork() {
-      return { title: 'Statistique', content: 'statistics' };
+    getAddNetwork(@Req() req: Request) {
+      const name = req.session.user.name;
+    const capitalizedname = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
+      return { 
+        title: 'Statistique',
+         content: 'statistics',
+         name: capitalizedname,
+        };
     }
 }
